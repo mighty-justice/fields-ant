6f42a95f9f1ab2e77d0b27b56fe19130784fc790
Robert Kuykendall <robert@rkuykendall.com>
2019-01-09 14:20:25 -0500

    Switch to default Ant form rendering

--- src/building-blocks/FormField.tsx
+++ src/building-blocks/FormField.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import autoBindMethods from 'class-autobind-decorator';
-import cx from 'classnames';
 
 import * as Antd from 'antd';
 
@@ -10,8 +9,6 @@ import { IFieldConfig } from '../interfaces';
 import {
   filterInsertIf,
 } from '../utilities/common';
-
-import Info, { Label, Value } from './Info';
 
 interface IProps {
   defaults?: object;
@@ -45,18 +42,11 @@ class FormField extends Component<IProps> {
     }
 
     return (
-      <Info key={fieldConfig.field}>
-        <Label className={cx({ 'form-item-required': fieldConfig.required })}>
-          {fieldConfig.label}
-        </Label>
-        <Value>
-          <Antd.Form.Item>
-            {getFieldDecorator(fieldConfig.field, decoratorOptions)(
-              <EditComponent {...editProps} />,
-            )}
-          </Antd.Form.Item>
-        </Value>
-      </Info>
+      <Antd.Form.Item label={fieldConfig.label}>
+        {getFieldDecorator(fieldConfig.field, decoratorOptions)(
+          <EditComponent {...editProps} />,
+        )}
+      </Antd.Form.Item>
     );
   }
 }
