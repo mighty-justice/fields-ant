8e57b8f6aafbd4a678722b65a331097319ab6d5d
Robert Kuykendall <robert@rkuykendall.com>
2019-01-10 16:43:48 -0500

    Create first tests for Card, EditableCard component (#18)
    * Create first test, for Card component
    
    * Add EditableCard test
    
    * CR Feedback:

--- test/Card.test.js
+++ test/Card.test.js
@@ -1,7 +1,32 @@
 /* global it, describe, expect */
+/* eslint-disable sort-keys */
+
+import React from 'react';
+import { mount } from 'enzyme';
+import faker from 'faker';
+
+import { Card } from '../src';
 
 describe('Card', () => {
-  it('Renders', async () => {
-    expect(true).toBe(true);
+  it('Renders loader', async () => {
+    const text = faker.lorem.sentence()
+      , title = faker.lorem.sentence()
+      , props = {
+        cardConfig: {
+          fieldSets: [[{ field: 'text' }]],
+          title,
+        },
+        model: { text },
+      };
+
+    const noLoading = mount(<Card {...props} />);
+    expect(noLoading.text()).toContain(title);
+    expect(noLoading.text()).toContain(text);
+    expect(noLoading.find('.ant-card').hasClass('ant-card-loading')).toBe(false);
+
+    const yesLoading = mount(<Card isLoading={true} {...props} />);
+    expect(yesLoading.text()).toContain(title);
+    expect(yesLoading.text()).not.toContain(text);
+    expect(yesLoading.find('.ant-card').hasClass('ant-card-loading')).toBe(true);
   });
 });
