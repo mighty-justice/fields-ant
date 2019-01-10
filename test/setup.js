110dae345cd5a14127fa6c958e3c1484fb305bd2
Robert Kuykendall <robert@rkuykendall.com>
2019-01-10 13:15:15 -0500

    Create first test, for Card component

--- test/setup.js
+++ test/setup.js
@@ -1,1 +1,12 @@
 import "@babel/polyfill";
+import enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { TesterConfig } from '@mighty-justice/tester';
+
+enzyme.configure({ adapter: new Adapter() });
+
+TesterConfig.configure(enzyme, {
+  hooks: [],
+  profiles: [],
+});
+
