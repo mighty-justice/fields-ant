589978224118dcd6926d9d1d50e08c9c0bd2d12c
Ryan Palermo <rpalermodrums@users.noreply.github.com>
2019-01-11 18:09:12 -0500

    Make FormFields component return a fragment, not a div (#26)

--- src/components/FormDrawer.tsx
+++ src/components/FormDrawer.tsx
@@ -74,12 +74,14 @@ class BaseFormDrawer extends Component<IProps> {
       >
         <Form layout='vertical' hideRequiredMark onSubmit={this.formManager.onSave}>
           {this.fieldSets.map((fieldSet, idx) => (
-            <FormFields
-              fieldSet={fieldSet}
-              form={form}
-              key={idx}
-              model={model}
-            />
+            <div>
+              <FormFields
+                fieldSet={fieldSet}
+                form={form}
+                key={idx}
+                model={model}
+              />
+            </div>
           ))}
           <Divider />
 
