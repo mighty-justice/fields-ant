a5c1029bd2d4d84556daff8735d168fbb7aaaa1b
Robert Kuykendall <robert@rkuykendall.com>
2019-01-16 18:05:42 -0500

    Self-CR

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,6 @@
 // `CheckerPlugin` is optional. Use it if you want async error reporting.
 // We need this plugin to detect a `--watch` mode. It may be removed later
 // after https://github.com/webpack/webpack/issues/3460 will be resolved.
-const { CheckerPlugin } = require('awesome-typescript-loader');
 const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
 
 module.exports = (baseConfig, env, defaultConfig) => {
@@ -13,17 +12,3 @@ module.exports = (baseConfig, env, defaultConfig) => {
   defaultConfig.resolve.extensions.push('.ts', '.tsx');
   return defaultConfig;
 };
-
-
-// module.exports = {
-//   // Currently we need to add '.ts' to the resolve.extensions array.
-//   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
-//   module: { rules: [
-//     { test: /\.tsx?$/, loader: 'awesome-typescript-loader', options: { transpileOnly: true } },
-//     { test: /\.css$/, use: ['style-loader','css-loader'] }
-//   ]},
-//   plugins: [
-//     new CheckerPlugin(),
-//     new TSDocgenPlugin(),
-//   ]
-// };
