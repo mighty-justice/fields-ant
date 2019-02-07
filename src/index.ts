55ac603a02cb46bddb91c0b673a0af21a55e9927
Robert Kuykendall <robert@rkuykendall.com>
2019-02-07 18:04:21 -0500

    Revert index

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,28 @@
 // Lower-level building blocks and helper components
-export { default as ButtonToolbar, IButtonToolbarProps } from './building-blocks/ButtonToolbar';
-export { default as CardField, ICardFieldProps } from './building-blocks/CardField';
-export { default as FormField, IFormFieldProps } from './building-blocks/FormField';
-export { default as FormFieldSet, IFormFieldSetProps } from './building-blocks/FormFieldSet';
+export { default as ButtonToolbar } from './building-blocks/ButtonToolbar';
+export { default as CardField } from './building-blocks/CardField';
+export { default as FormField } from './building-blocks/FormField';
+export { default as FormFieldSet } from './building-blocks/FormFieldSet';
 export { default as GuardedButton } from './building-blocks/GuardedButton';
 export { default as Info, Label, Value, CARD_COL_LABEL, CARD_COL_VALUE } from './building-blocks/Info';
-export { default as NestedFieldSet, INestedFieldSetProps } from './building-blocks/NestedFieldSet';
+export { default as NestedFieldSet } from './building-blocks/NestedFieldSet';
 
 // Components
-export { default as ArrayCard, IArrayCardProps } from './components/ArrayCard';
-export { default as Card, ICardProps } from './components/Card';
-export { default as EditableArrayCard, IEditableArrayCardProps } from './components/EditableArrayCard';
-export { default as EditableCard, IEditableCardProps } from './components/EditableCard';
-export { default as FormCard, IFormCardProps } from './components/FormCard';
-export { default as FormDrawer, IFormDrawerProps } from './components/FormDrawer';
-export { default as FormModal, IFormModalProps } from './components/FormModal';
-export { default as SummaryCard, ISummaryCardProps } from './components/SummaryCard';
+export { default as ArrayCard } from './components/ArrayCard';
+export { default as Card } from './components/Card';
+export { default as EditableArrayCard } from './components/EditableArrayCard';
+export { default as EditableCard } from './components/EditableCard';
+export { default as FormCard } from './components/FormCard';
+export { default as FormDrawer } from './components/FormDrawer';
+export { default as FormModal } from './components/FormModal';
+export { default as SummaryCard } from './components/SummaryCard';
 
 // Form inputs
-export { default as ObjectSearchCreate, IObjectSearchCreateProps } from './inputs/ObjectSearchCreate';
-export { default as OptionSelect, IOptionSelectProps } from './inputs/OptionSelect';
+export { default as ObjectSearchCreate } from './inputs/ObjectSearchCreate';
+export { default as OptionSelect } from './inputs/OptionSelect';
+export { default as OptionSelectDisplay, formatOptionSelect } from './inputs/OptionSelectDisplay';
 export { default as RadioGroup } from './inputs/RadioGroup';
 export { default as Rate, formatRating } from './inputs/Rate';
-export {
-  default as OptionSelectDisplay,
-  formatOptionSelect,
-  IOptionSelectDisplayProps,
-} from './inputs/OptionSelectDisplay';
 
 // Utility classes and functions
 export { default as FormManager } from './utilities/FormManager';
