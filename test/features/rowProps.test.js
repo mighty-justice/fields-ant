13d4a8529be43afb5a98515d98f363abca52f0c5
Robert Kuykendall <robert@rkuykendall.com>
2019-02-11 12:07:31 -0500

    Cover colProps and rowProps

---
+++ test/features/rowProps.test.js
@@ -0,0 +1,64 @@
+/* global it, describe, expect */
+import React from 'react';
+import { Tester } from '@mighty-justice/tester';
+
+import { COMPONENT_GENERATORS } from '../factories';
+
+const SUPPORTING_COMPONENTS = [
+  'FormCard',
+];
+
+const rowProps = { gutter: 32 }
+  , fieldSetsWithout = [
+    {
+      fields: [
+        { field: 'plaintiff.first_name' },
+        { field: 'plaintiff.last_name' },
+      ],
+      legend: 'Who is the plaintiff?',
+    },
+    {
+      fields: [
+        { field: 'attorney.first_name' },
+        { field: 'attorney.last_name' },
+      ],
+      legend: 'Who is the attorney?',
+    }
+  ]
+  , fieldSetsWith = [
+    {
+      fields: [
+        { field: 'plaintiff.first_name' },
+        { field: 'plaintiff.last_name' },
+      ],
+      legend: 'Who is the plaintiff?',
+      rowProps,
+    },
+    {
+      fields: [
+        { field: 'attorney.first_name' },
+        { field: 'attorney.last_name' },
+      ],
+      legend: 'Who is the attorney?',
+      rowProps,
+    }
+  ]
+  ;
+
+describe('colProps', () => {
+  SUPPORTING_COMPONENTS.forEach(componentName => {
+    it(`Renders colProps in ${componentName}`, async () => {
+      const { Component, propsFactory } = COMPONENT_GENERATORS[componentName]
+        , propsWithout = propsFactory.build({ fieldSets: fieldSetsWithout })
+        , propsWith = { ...propsWithout, fieldSets: fieldSetsWith }
+        , expression = 'margin-left: -16px; margin-right: -16px;'
+        ;
+
+      const testerWithout = await new Tester(Component, { props: propsWithout }).mount();
+      expect(testerWithout.html()).not.toContain(expression);
+
+      const testerWith = await new Tester(Component, { props: propsWith }).mount();
+      expect(testerWith.html()).toContain(expression);
+    });
+  });
+});
