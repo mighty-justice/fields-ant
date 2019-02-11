e7f8dceb3ca315f78037a3f1a4b4c7a1223ba7f1
Robert Kuykendall <robert@rkuykendall.com>
2019-02-11 12:08:57 -0500

    Remove dumb ArrayCard dissapearing feature

--- src/components/ArrayCard.tsx
+++ src/components/ArrayCard.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import { isEmpty } from 'lodash';
 
 import * as Antd from 'antd';
 
@@ -16,7 +15,6 @@ export interface IArrayCardProps extends ICardProps {
 class ArrayCard extends Component<IArrayCardProps> {
   public render () {
     const { title, renderTopRight, isLoading, model, fieldSets, classNameSuffix } = this.props;
-    if (isEmpty(model)) { return null; }
 
     return (
       <Antd.Card title={title} extra={renderTopRight && renderTopRight()} loading={isLoading}>
