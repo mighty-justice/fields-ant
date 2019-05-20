b8a22e21e9ba1a28a830a1ceae1e5c5d766fb6ca
Robert Kuykendall <robert@rkuykendall.com>
2019-05-20 18:00:29 -0400

    Add ModalComponent prop to EditableCard

--- test/components/EditableCard.test.tsx
+++ test/components/EditableCard.test.tsx
@@ -2,30 +2,43 @@
 import faker from 'faker';
 import { Tester } from '@mighty-justice/tester';
 
-import { EditableCard } from '../../src';
+import { EditableCard, FormDrawer, FormModal } from '../../src';
 import { editableCardPropsFactory } from '../factories';
 
 describe('EditableCard', () => {
-  it('Edits text', async () => {
-    const text = faker.lorem.sentence()
-      , newText = faker.lorem.sentence()
-      , onSave = jest.fn().mockResolvedValue({})
-      , props = {
-        ...editableCardPropsFactory.build(),
-        fieldSets: [[{ field: 'text', type: 'string' }]],
-        model: { text },
-        onSave,
-      };
+  [
+    { where: 'inline', whereProps: {} },
+    { where: 'in a FormModal', whereProps: { ModalComponent: FormModal } },
+    { where: 'in a formDrawer', whereProps: { ModalComponent: FormDrawer } },
+  ].forEach(({ where, whereProps }) => {
+    it(`Edits text ${where}`, async () => {
+      const text = faker.lorem.sentence()
+        , newText = faker.lorem.sentence()
+        , onSave = jest.fn().mockResolvedValue({})
+        , props = {
+          ...editableCardPropsFactory.build(),
+          fieldSets: [[{ field: 'text', type: 'string' }]],
+          model: { text },
+          onSave,
+          ...whereProps,
+        };
 
-    const tester = await new Tester(EditableCard, { props }).mount();
+      const tester = await new Tester(EditableCard, { props }).mount();
 
-    tester.click(`button.btn-edit`);
-    tester.changeInput('input', newText);
+      if (whereProps.ModalComponent) {
+        expect(tester.find(whereProps.ModalComponent).length).toBe(1);
+      }
 
-    expect(onSave).not.toHaveBeenCalled();
-    tester.submit();
-    await tester.sleep();
-    expect(onSave).toHaveBeenCalledWith({ text: newText });
+      tester.click(`button.btn-edit`);
+      tester.changeInput('input', newText);
+
+      expect(tester.find('FormCard').length).toBe(whereProps.ModalComponent ? 0 : 1);
+
+      expect(onSave).not.toHaveBeenCalled();
+      tester.submit();
+      await tester.sleep();
+      expect(onSave).toHaveBeenCalledWith({ text: newText });
+    });
   });
 
   it('Can be deleted', async () => {
