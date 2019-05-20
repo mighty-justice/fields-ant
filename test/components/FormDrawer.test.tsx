e772424842307ad8624cf9e27bf9a476d93b6c54
Robert Kuykendall <robert@rkuykendall.com>
2019-05-20 16:14:47 -0400

    Coverage

--- test/components/FormDrawer.test.tsx
+++ test/components/FormDrawer.test.tsx
@@ -1,23 +1,5 @@
-import faker from 'faker';
-
-import { FormDrawer } from '../../src';
-import { Tester } from '@mighty-justice/tester';
-import SmartBool from '@mighty-justice/smart-bool';
-
 describe('FormDrawer', () => {
-  it('Renders', async () => {
-    const text = faker.lorem.sentence()
-      , title = faker.lorem.sentence()
-      , props = {
-        fieldSets: [[{ field: 'text' }]],
-        isVisible: new SmartBool(true),
-        model: { text },
-        title,
-      };
-
-    const tester = await new Tester(FormDrawer, { props }).mount();
-    expect(tester.text()).toContain(title);
-    expect(tester.text()).toContain('Text');
-    expect(tester.html()).toContain(text);
+  it('is tested the same as FormModal', async () => {
+    // See FormModal.test.tsx
   });
 });
--- test/components/FormModal.test.tsx
+++ test/components/FormModal.test.tsx
@@ -1,36 +1,112 @@
 import faker from 'faker';
 
-import { FormModal } from '../../src';
 import { Tester } from '@mighty-justice/tester';
 
-describe('FormModal', () => {
-  it('Renders', async () => {
-    const text = faker.lorem.sentence()
-      , title = faker.lorem.sentence()
-      , props = {
-        fieldSets: [[{ field: 'text' }]],
-        model: { text },
-        title,
-      };
+import { COMPONENT_GENERATORS } from '../factories';
+import SmartBool from '@mighty-justice/smart-bool';
 
-    const tester = await new Tester(FormModal, { props }).mount();
-    expect(tester.text()).toContain(title);
-    expect(tester.text()).toContain('Text');
-    expect(tester.html()).toContain(text);
-  });
+[
+  {
+    name: 'FormModal',
+  },
+  {
+    name: 'FormDrawer',
+  },
+].forEach(({ name }) => {
+  describe(name, () => {
+    it('Renders', async () => {
+      const { ComponentClass, propsFactory } = COMPONENT_GENERATORS[name]
+        , exampleValue = faker.lorem.sentence()
+        , title = faker.lorem.sentence()
+        , props = {
+          ...propsFactory.build(),
+          fieldSets: [[{ field: 'exampleField' }]],
+          model: { exampleField: exampleValue },
+          title,
+        };
 
-  it('Renders children', async () => {
-    const textChildren = faker.lorem.sentence()
-      , textChildrenBefore = faker.lorem.sentence()
-      , props = {
-        children: textChildren,
-        childrenBefore: textChildrenBefore,
-        fieldSets: [],
-        title: 'Children Modal',
-      };
+      const tester = await new Tester(ComponentClass, { props }).mount();
+      expect(tester.text()).toContain(title);
+      expect(tester.text()).toContain('Example Field');
+      expect(tester.html()).toContain(exampleValue);
+    });
 
-    const tester = await new Tester(FormModal, { props }).mount();
-    expect(tester.text()).toContain(textChildren);
-    expect(tester.text()).toContain(textChildrenBefore);
+    it('Renders children', async () => {
+      const { ComponentClass, propsFactory } = COMPONENT_GENERATORS[name]
+        , textChildren = faker.lorem.sentence()
+        , textChildrenBefore = faker.lorem.sentence()
+        , props = {
+          ...propsFactory.build(),
+          children: textChildren,
+          childrenBefore: textChildrenBefore,
+        };
+
+      const tester = await new Tester(ComponentClass, { props }).mount();
+      expect(tester.text()).toContain(textChildren);
+      expect(tester.text()).toContain(textChildrenBefore);
+    });
+
+    it('Works with isVisible', async () => {
+      const { ComponentClass, propsFactory } = COMPONENT_GENERATORS[name]
+        , title = faker.lorem.sentence()
+        , isVisible = new SmartBool(false)
+        , props = {
+          ...propsFactory.build(),
+          isVisible,
+          title,
+        };
+
+      const tester = await new Tester(ComponentClass, { props }).mount({});
+
+      // Open modal using isVisible
+      expect(tester.text()).toBe(null);
+      isVisible.setTrue();
+      expect(tester.text()).toContain(title);
+
+      // Check that it auto-closes on successful submit
+      await tester.refresh();
+      await tester.submit();
+      expect(isVisible.isTrue).toBe(false);
+
+      // Re-open and test cancel button
+      isVisible.setTrue();
+      tester.click('button > span[children="Cancel"]');
+      expect(isVisible.isTrue).toBe(false);
+    });
+
+    it('Works with onSuccess and onCancel', async () => {
+      const { ComponentClass, propsFactory } = COMPONENT_GENERATORS[name]
+        , title = faker.lorem.sentence()
+        , onCancel = jest.fn()
+        , onSuccess = jest.fn()
+        , isVisible = new SmartBool(true)
+        , props = {
+          ...propsFactory.build(),
+          isVisible,
+          onCancel,
+          onSuccess,
+          title,
+        };
+
+      const tester = await new Tester(ComponentClass, { props }).mount();
+      expect(onCancel).not.toHaveBeenCalled();
+      expect(onSuccess).not.toHaveBeenCalled();
+
+      // Test submit button
+      onCancel.mockClear();
+      onSuccess.mockClear();
+      await tester.submit();
+      expect(onCancel).not.toHaveBeenCalled();
+      expect(onSuccess).toHaveBeenCalled();
+      expect(isVisible.isTrue).toBe(true);
+
+      // Test cancel button
+      onCancel.mockClear();
+      onSuccess.mockClear();
+      tester.click('button > span[children="Cancel"]');
+      expect(onCancel).toHaveBeenCalled();
+      expect(onSuccess).not.toHaveBeenCalled();
+      expect(isVisible.isTrue).toBe(true);
+    });
   });
 });
