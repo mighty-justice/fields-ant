d304e1e5b8924bc28b1981a944dd59751f089f26
Robert Kuykendall <robert@rkuykendall.com>
2019-05-28 11:17:36 -0400

    Add explanation

--- src/inputs/Hidden.tsx
+++ src/inputs/Hidden.tsx
@@ -11,6 +11,16 @@ import {
   IInputProps,
 } from '../interfaces';
 
+/*
+Most components are automatically wrapped with a lot of boiler-plate form code
+in FormItem.tsx. Components that want more control over this can pass
+skipFieldDecorator and then use FormItem themselves. For hidden inputs, we want
+almost no wrapping at all so the component doesn't render anything on the page.
+To do this, we use skipFieldDecorator to opt out of boiler-plate then
+fieldConfigProp to get some advanced props ( like fieldConfig ) so that we can
+do some of what FormItem does for ourselves below:
+*/
+
 @autoBindMethods
 @observer
 class Hidden extends Component<IInputProps> {
