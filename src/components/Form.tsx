4add8714337f7e318b5227ecdd6cfecd011829f1
Karen Ka <karen@mighty.com>
2019-05-29 17:25:50 -0400

    fix function styling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -71,8 +71,9 @@ export class UnwrappedForm extends Component<IFormWrappedProps> {
     return fillInFieldSets(this.props.fieldSets);
   }
 
-  private hasErrors = (fieldsError: any) =>
-    Object.keys(fieldsError).some((field) => fieldsError[field])
+  private hasErrors (fieldsError: any) {
+    return Object.keys(fieldsError).some((field) => fieldsError[field]);
+  }
 
   private renderControls () {
     const {
