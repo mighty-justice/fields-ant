1aecbdb35b95826cc9c15fd9d011b7622d68d4ba
Robert Kuykendall <robert@rkuykendall.com>
2019-09-06 13:07:16 -0400

    Fix docs building

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,15 @@
 // `CheckerPlugin` is optional. Use it if you want async error reporting.
 // We need this plugin to detect a `--watch` mode. It may be removed later
 // after https://github.com/webpack/webpack/issues/3460 will be resolved.
-const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
 
 module.exports = ({ config, mode }) => {
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve('awesome-typescript-loader')
+    test: /\.tsx?$/,
+    use: [
+      require.resolve('awesome-typescript-loader'),
+      require.resolve('react-docgen-typescript-loader'),
+    ],
   });
-
-  config.plugins.push(new TSDocgenPlugin());
 
   config.resolve.extensions.push('.ts', '.tsx');
 
