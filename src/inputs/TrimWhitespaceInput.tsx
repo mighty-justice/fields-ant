8aa9025377f03cc3104836172c83de485caaf0f9
Ryan Palermo <rpalermodrums@users.noreply.github.com>
2019-11-06 18:38:23 -0500

    Add trim whitespace component and apply to url and email fields (#248)

---
+++ src/inputs/TrimWhitespaceInput.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+import autoBindMethods from 'class-autobind-decorator';
+
+import { Input } from 'antd';
+
+import { IAntFormField, IInjected, IInputProps } from '../interfaces';
+
+@autoBindMethods
+@observer
+class TrimWhitespaceInput extends Component<{}> {
+  @observable private value = '';
+
+  private get injected () {
+    return this.props as IInjected & IInputProps & IAntFormField;
+  }
+
+  private onChange (e: any) {
+    this.value = e.target.value.trim();
+    this.injected.onChange(this.value);
+  }
+
+  public render () {
+    return (
+      <Input value={this.value} {...this.props} onChange={this.onChange} />
+    );
+  }
+}
+
+export default TrimWhitespaceInput;
