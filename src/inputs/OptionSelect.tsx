11b0be9ffe560e72390e1e02fdaf58b118a2f0bb
Robert Kuykendall <robert@rkuykendall.com>
2020-01-15 16:19:17 -0500

    Remove unnecesary onChange from OptionSelect

--- src/inputs/OptionSelect.tsx
+++ src/inputs/OptionSelect.tsx
@@ -2,31 +2,15 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import autoBindMethods from 'class-autobind-decorator';
 
-import { IInputProps, IOption } from '../interfaces';
+import { IInputProps } from '../interfaces';
 
 import ObjectSelect from './ObjectSelect';
-import { IAntFormField, IInjected } from '../index';
 
 @autoBindMethods
 @observer
 class OptionSelect extends Component<IInputProps> {
-  private get injected () {
-    return this.props as IInjected & IInputProps & IAntFormField;
-  }
-
-  private onChange (option?: null | IOption) {
-    const { onChange } = this.injected;
-    return onChange(option ? option.value : option);
-  }
-
   public render () {
-    const props = {
-      ...this.props,
-      keyBy: 'value',
-      onChange: this.onChange,
-    };
-
-    return <ObjectSelect {...props} />;
+    return <ObjectSelect {...this.props} keyBy='value' />;
   }
 }
 
