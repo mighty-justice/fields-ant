d4413277bc4d206fa53ae61bfb6aa12d43f51f48
Robert Kuykendall <robert@rkuykendall.com>
2020-03-26 14:58:48 -0400

    Just the change requested in CR

--- src/building-blocks/FieldSet.tsx
+++ src/building-blocks/FieldSet.tsx
@@ -8,6 +8,7 @@ import * as Antd from 'antd';
 import { IFieldSetPartial } from '../interfaces';
 import { IClassName } from '../props';
 import { isPartialFieldSetSimple } from '../utilities';
+import { CLASS_PREFIX } from '../consts';
 
 import Legend from './Legend';
 
@@ -15,6 +16,8 @@ interface IProps {
   fieldSet: IFieldSetPartial;
   className?: IClassName;
 }
+
+const CLASS_NAME = `${CLASS_PREFIX}-field-set`;
 
 @autoBindMethods
 @observer
@@ -25,7 +28,7 @@ export default class FieldSet extends Component<IProps> {
       ;
 
     return (
-      <div className={cx('fields-ant-field-set', className)}>
+      <div className={cx(CLASS_NAME, className)}>
         <Antd.Row {...rowProps}>
           <Legend fieldSet={fieldSet} />
 
