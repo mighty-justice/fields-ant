f8a0a53ab87166df84ba117f8e2d54756f3c5a45
Will Saffran <willh.saffran@gmail.com>
2021-04-24 16:32:49 -0400

    Add card border property

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,9 +9,11 @@ import * as Antd from 'antd';
 import { fillInFieldSets, filterFieldSets } from '../utilities';
 import CardFieldSet from '../building-blocks/CardFieldSet';
 import { ISharedComponentProps } from '../props';
+import { cardPropsDefaults } from '../propsDefaults';
 import { CLASS_PREFIX } from '../consts';
 
 export interface ICardProps extends ISharedComponentProps {
+  bordered?: boolean;
   renderTopRight?: () => any;
 }
 
@@ -20,17 +22,20 @@ const CLASS_NAME = `${CLASS_PREFIX}-card`;
 @autoBindMethods
 @observer
 class Card extends Component<ICardProps> {
+  public static defaultProps: Partial<ICardProps> = { ...cardPropsDefaults };
+
   @computed
   private get fieldSets() {
     return fillInFieldSets(this.props.fieldSets);
   }
 
   public render() {
-    const { className, title, renderTopRight, isLoading, model, ...passDownProps } = this.props,
+    const { bordered, className, title, renderTopRight, isLoading, model, ...passDownProps } = this.props,
       filteredFieldSets = filterFieldSets(this.fieldSets, { model, writeOnly: true });
 
     return (
       <Antd.Card
+        bordered={bordered}
         className={cx(CLASS_NAME, className)}
         extra={renderTopRight && renderTopRight()}
         loading={isLoading}
