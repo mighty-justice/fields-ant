d24d9d9cb3682c74f0ab31ab9614d90522d6f446
Robert Kuykendall <robert@rkuykendall.com>
2021-04-26 12:05:40 -0400

    Fix setRefFormManager not called by FormModal

---
+++ test/features/setRefFormManager.test.ts
@@ -0,0 +1,15 @@
+import { Tester } from '@mighty-justice/tester';
+import { COMPONENT_GENERATORS, FORM_COMPONENTS } from '../factories';
+
+describe('Renders', () => {
+  FORM_COMPONENTS.forEach(componentName => {
+    it(`${componentName} calls setRefFormManager`, async () => {
+      const { ComponentClass, propsFactory } = COMPONENT_GENERATORS[componentName],
+        setRefFormManager = jest.fn(),
+        props = propsFactory.build({ setRefFormManager });
+
+      await new Tester(ComponentClass, { props }).mount();
+      expect(setRefFormManager).toHaveBeenCalledTimes(1);
+    });
+  });
+});
