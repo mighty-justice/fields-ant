2e81050cb7a915cde13cdc344cf3a1f2ebdd2c55
Robert Kuykendall <robert@rkuykendall.com>
2021-04-26 12:33:53 -0400

    Update setRefFormManager.test.ts

--- test/features/setRefFormManager.test.ts
+++ test/features/setRefFormManager.test.ts
@@ -1,7 +1,7 @@
 import { Tester } from '@mighty-justice/tester';
 import { COMPONENT_GENERATORS, FORM_COMPONENTS } from '../factories';
 
-describe('Renders', () => {
+describe('setRefFormManager', () => {
   FORM_COMPONENTS.forEach(componentName => {
     it(`${componentName} calls setRefFormManager`, async () => {
       const { ComponentClass, propsFactory } = COMPONENT_GENERATORS[componentName],
